d842ffd3820216369cfca8971ad352fbad8306b0
Luan Vieira <luanvieira.er@gmail.com>
2020-08-18 19:09:09 -0300

    Commit base para o Projeto SOAP

---
+++ index.js
@@ -0,0 +1,8 @@
+process.title = 'WebServerSOAP';
+var args = process.argv,
+port = args[2] || 7070,
+webServer = require('./server');
+
+webServer.listen(port, function() {
+console.log('Server started at port ' + port);
+});
