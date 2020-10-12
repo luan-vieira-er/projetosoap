d4d5e193bdc524dd02c396baea852f66963026bd
Luan Vieira <luanvieira.er@gmail.com>
2020-10-12 15:16:22 -0300

    Correcao do Start e Porta

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 process.title = 'WebServerSOAP';
-var args = process.env.PORT,
-port = args[2] || 7070,
+port = process.env.PORT || 7070,
 webServer = require('./server');
 
 webServer.listen(port, function() {
