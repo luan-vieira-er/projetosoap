97cceb4366bbf893bd1e06cd1b3fe31ac0d9e5f5
Luan Vieira <luanvieira.er@gmail.com>
2020-10-13 17:25:46 -0300

    Readequacao para o WSDL do SOAP

--- server.js
+++ server.js
@@ -1,51 +1,35 @@
-var http = require('http'),
-  config = require('./config'),
-  fileHandler = require('./filehandler'),
-  parse = require('url').parse,
-  types = config.types,
-  rootFolder = config.rootFolder,
-  defaultIndex = config.defaultIndex,
-  server;
+"use strict";
 
- module.exports = server = http.createServer();
+var soap = require('soap');
+var http = require('http');
 
- server.on('request', onRequest);
+var myService = {
+    ws: {
+        pi: {
+            newPi : function(args) {
+                var n = 10000000, inside = 0, x, y, i;
+                for (i = 0; i<n; i++){
+                    x = Math.random();
+                    y = Math.random();
+                    if (Math.sqrt(x * x + y * y) <= 1){
+                        inside++;
+                    }
+                }
+                var v = 4 * inside / n;
+                console.log(v);  
+                    return { v };
 
- function onRequest(req, res) {
-    console.log("Inicializado Server SOAP - Pi Estimado: " + getPi()) ;
-    //res.send("Pi Estimado: " + getPi());
-
-  var filename = parse(req.url).pathname,
-      fullPath,
-      extension;
-
-  if(filename === '/') {
-      filename = defaultIndex;
-  }
-  fullPath = rootFolder + filename;
-  extension = filename.substr(filename.lastIndexOf('.') + 1);
-
-  fileHandler(fullPath, function(data) {
-      res.writeHead(200, {
-           'Content-Type': types[extension] || 'text/plain',
-           'Content-Length': data.length
-      });
-      res.end(data);
-
-  }, function(err) {
-      res.writeHead(404);
-      res.end();
-  });
- }
-
- function getPi(){
-    var n = 10000000, inside = 0, x, y, z;
-    for (i = 0; i<n; i++){
-        x = Math.random();
-        y = Math.random();
-        if (Math.sqrt(x * x + y * y) <= 1){
-            inside++;
+                    
+            }
         }
     }
-    return 4 * inside / n;
-}
+};
+
+var xml = require('fs').readFileSync('myservice.wsdl', 'utf8');
+
+var server = http.createServer(function(request,response) {
+    response.end("404: Not Found: "+request.url);
+});
+
+server.listen(7070);
+soap.listen(server, '/myservice', myService, xml);
